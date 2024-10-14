3db87471d573f7ab89d663ff00e1bcf558f9f1cb
pivnati <natanmuleta911@gmail.com>
2024-10-13 18:48:20 -0700

    create the category section

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -6,9 +6,7 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+   protected $category;
     public function index()
     {
         //
